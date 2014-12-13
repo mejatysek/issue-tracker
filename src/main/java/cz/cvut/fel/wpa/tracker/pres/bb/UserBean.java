9a0e6f8453642589f4a1a302f4915d11fea66c46
Adam Uhlir <uhlir.a@gmail.com>
2014-12-13 15:14:59 +0100

    User listing

--- src/main/java/cz/cvut/fel/wpa/tracker/pres/bb/UserBean.java
+++ src/main/java/cz/cvut/fel/wpa/tracker/pres/bb/UserBean.java
@@ -1,6 +1,8 @@
 package cz.cvut.fel.wpa.tracker.pres.bb;
 
+import cz.cvut.fel.wpa.tracker.dto.RoleDto;
 import cz.cvut.fel.wpa.tracker.dto.UserDto;
+import cz.cvut.fel.wpa.tracker.service.RoleService;
 import cz.cvut.fel.wpa.tracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -19,5 +21,12 @@ public class UserBean {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
     public List<UserDto> getUsers() { return userService.getAllUsers(); }
+
+    public RoleDto getRole(Long id){ return roleService.getRoleById(id); }
+
+    public void deactivateUser(Long id) { userService.deactivateUser(id); }
 }
